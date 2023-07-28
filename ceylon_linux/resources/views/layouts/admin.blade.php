e1c41d65e8fcaa9d13c05c740bea09dbdc0efa20
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-28 07:52:58 +0530

    Initial commit

---
+++ ceylon_linux/resources/views/layouts/admin.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+
+
+</head>
+
+
+<body>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <p class="font-weight-bold text-right mr-5">Welcome System Admin<br>current date</p>
+
+    <!-- Zone Registration -->
+    <div class="container">
+        <h6 class="text-center"><b>ADD ZONE</b></h6>
+        <div class="row justify-content-center">
+            <div class="col-sm-6">
+                <form action="/admin" method="POST">
+
+                    <div class="form-group row">
+                        <label for="zoneCode" class="col-sm-4 col-form-label">Zone Code</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="zoneCode" placeholder="Automatically">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="zlDescription" class="col-sm-4 col-form-label">Zone Long Description</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="zlDescription" placeholder="Ex:ZONE 1">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="shortDescription" class="col-sm-4 col-form-label">Short Description</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="shortDescription" placeholder="Ex: Z01">
+                        </div>
+                    </div>
+                </form>
+                <div class="text-center">
+                    <button type="button" class="btn btn-success col-sm-3">SAVE</button>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
