1f5ea0beb58bbf3c2621ba496a0a0bd72f546c55
Amaya-2001 <amawickramasinghe2001@gmail.com>
2023-07-29 02:55:06 +0530

    Added Product Order table

--- ceylon_linux/resources/views/add-product-order.blade.php
+++ ceylon_linux/resources/views/add-product-order.blade.php
@@ -4,11 +4,7 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-
-
 </head>
-
 
 <body>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -17,17 +13,18 @@
 
     <!-- Zone Registration -->
     <div class="container">
-        <h6 class="text-center"><b>ADD INDIVIDUAL PURCHASE ORDER</b></h6>
+        <h5 class="text-center"><b>ADD INDIVIDUAL PURCHASE ORDER</b></h5>
         <div class="row justify-content-center">
             <div class="col">
                 <form action="product_order" method="POST">
+                    @csrf
                     <div class="row">
                         <div class="col">
                             <div class="form-group row">
                                 <label for="zone" class=" col-form-label">Zone </label>
                                 <div class="col">
                                     <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                                        <option selected></option>
+                                        <option selected>Select</option>
                                         <option value="ZONE1">ZONE1</option>
                                         <option value="ZONE2">ZONE2</option>
                                         <option value="ZONE3">ZONE3</option>
@@ -75,18 +72,56 @@
                             </div>
                         </div>
                     </div>
-                </form>
+                    <!-- second row -->
+                    <div class="row">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-sm">
+                                    <div class="form-group row">
+                                        <label for="date" class=" col-form-label">Date</label>
+                                        <div class="col">
+                                            <input type="date" class="form-control" name="date" placeholder="Automatically">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm">
+                                    <div class="form-group row">
+                                        <label for="po_no" class=" col-form-label">PO No</label>
+                                        <div class="col">
+                                            <input type="text" class="form-control" name="po_no" placeholder="Automatically">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm">
+                                    <div class="form-group row">
+                                        <label for="remark" class=" col-form-label">Remark</label>
+                                        <div class="col">
+                                            <input type="text" class="form-control" name="remark" placeholder="Type">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
+                    <!-- Add PO table -->
+                    <div class="row">
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="SKU CODE"></div>
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="SKU NAME"></div>
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="UNIT PRICE"></div>
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="AVB QTY"></div>
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="ENTER QTY"></div>
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="UNITS"></div>
+                        <div class="col"><input type="text" class="form-control" name="remark" placeholder="TOTAL PRICE"></div>
+
+                    </div>
+                    <!-- Add Product Order button -->
+                    <div class="text-center mt-5">
+                        <button type="submit" class="btn btn-success col-sm-3">Add PO</button>
+                    </div>
+                </form>
             </div>
         </div>
-
-
-
-
-
-
-
-
 </body>
 
 </html>
